5c4848be61d59bc28759e36e9a05c66c4541192c
Michelle <jhmichellekim@gmail.com>
2016-10-22 23:54:43 -0700

    finished more-practice inserting and creating tables

--- sql/more-practice.sql
+++ sql/more-practice.sql
@@ -4,8 +4,25 @@
 
 -- INSERT
 
+INSERT INTO models (year, brand_name, name)
+    VALUES (2015, 'Chevrolet', 'Malibu');
+
+INSERT INTO models (year, brand_name, name)
+    VALUES (2015, 'Subaru', 'Outback');
 
 -- CREATE TABLE
 
+CREATE TABLE Awards(
+    name varchar(30)    NOT NULL,
+    year int            NOT NULL,
+    winner_id int       NOT NULL
+);
+
 
 -- More INSERT
+
+INSERT INTO awards (name, year, winner_id)
+    VALUES ('IIHS Safety Award', 2015, 49);
+
+INSERT INTO awards (name, year, winner_id)
+    VALUES ('IIHS Safety Award', 2015, 50);
